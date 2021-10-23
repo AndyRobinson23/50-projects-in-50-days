7af86217a64f72ae77cbef65c12b249d5766f672
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-10-23 05:53:32 +0100

    completed day 4 - hidden search

---
+++ Day 4 - Hidden Search/hidden.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const searchEl = document.querySelector('.search');
+const btnEl = document.querySelector('.btn'); 
+const inputEl = document.querySelector('.input');
+
+btnEl.addEventListener('click', () => {
+    searchEl.classList.toggle('active');
+    inputEl.focus();
+});
