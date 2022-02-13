c0f38e5a5e9ef31049922ed49f46ced79544f1b7
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2022-02-13 15:49:25 +0000

    added js functionality to make content in profile cards dynamic

--- Day 28 - GitHub Profiles/script.js
+++ Day 28 - GitHub Profiles/script.js
@@ -1,12 +1,48 @@
 const APIURL = 'https://api.github.com/users/';
 const formEl = document.querySelector('.form');
 const formInputEl = document.querySelector('.form input');
+const mainEl = document.getElementById('main');
 
+// FIXME: error handling does not work, shows the profile card with undefined values instead of the error card
 const getUser = function(username) {
-    fetch(APIURL + username).then(response => response.json()).then(data => console.log(data)).catch(err => console.error(err));
+    fetch(APIURL + username).then(response => response.json()).then(data => createUserCard(data)).catch(err => {
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username');
+        }
+    });
 }
 
-// getUser('AndyRobinson23');
+const createUserCard = function(userData) {
+    const cardHTML = `
+        <div class="profile-card">
+            <div class="profile-card__left">
+                <img src=${userData.avatar_url} />
+            </div>
+            <div class="profile-card__right">
+                <h2 class="profile-card__name">${userData.login}</h2>
+                <p class="profile-card__bio">${userData.bio}</p>
+                <ul class="profile-card__stats">
+                    <li class="profile-card__stat"><span class="profile-card__stat--number">${userData.followers}</span> followers</li>
+                    <li class="profile-card__stat"><span class="profile-card__stat--number">${userData.following}</span> following</li>
+                    <li class="profile-card__stat"><span class="profile-card__stat--number">${userData.public_repos}</span> Repos</li>
+                </ul>
+                <div id="repo"></div>
+            </div>
+        </div>
+    `
+
+    mainEl.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="profile-card">
+            <h1>${msg}</h1>
+        </div>
+    `;
+
+    mainEl.innerHTML = cardHTML;
+}
 
 formEl.addEventListener('submit', e => {
     e.preventDefault();
