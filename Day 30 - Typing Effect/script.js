add630667e19e86c3b26f6b7f12a17ee27ae9faa
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2022-02-16 11:48:48 +0000

    added comments to help understand what the code is doing

--- Day 30 - Typing Effect/script.js
+++ Day 30 - Typing Effect/script.js
@@ -1,5 +1,7 @@
 const textEl = document.getElementById('text');
 const speedEl = document.getElementById('speed');
+
+// Initial settings
 const text = 'We love programming!';
 let idx = 1;
 let speed = 300 / speedEl.value;
@@ -7,15 +9,20 @@ let speed = 300 / speedEl.value;
 writeText();
 
 function writeText() {
+    // Output the first letter of the text to the screen
     textEl.innerText = text.slice(0, idx);
 
+    // Increase the index by one in order to move to the next letter
     idx++;
 
+    // Check if we've hit the end of the letters. If so, reset the index to the first letter
     if(idx > text.length) idx = 1;
 
+    // Call this function again every number of seconds defined by the 'speed' input
     setTimeout(writeText, speed);
 };
 
+// Update the speed when the Speed input element is adjusted
 speedEl.addEventListener('input', (e) => {
     speed = 300 / e.target.value;
 })
